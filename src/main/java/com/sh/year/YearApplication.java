71c7019e1bb024470d30ba0ee1d42e12e1e31506
OOOIOOOIO <polite159@gmail.com>
2024-03-04 22:49:00 +0900

    init

---
+++ src/main/java/com/sh/year/YearApplication.java
@@ -0,0 +1,13 @@
+package com.sh.year;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/year/YearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.year;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
